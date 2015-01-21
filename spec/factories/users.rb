4d648990d2edfb94116638d91ffbef09664fd23d
micah rich <micah@micahrich.com>
2015-01-20 22:07:22 -0800

    basic name test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+     name 'Dave Franco'
+    email 'dave@franco.com'
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  it "has a name"
+  it "has a name" do
+    user = FactoryGirl.build(:user)
+    expect(user.name).not_to be_nil
+  end
   it "has a first name"
   it "has a last name"
   it "has an email address"
