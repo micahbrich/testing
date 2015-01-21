550bc9ae4901c383681eae17c21d36eb9bd2a726
micah rich <micah@micahrich.com>
2015-01-20 22:10:58 -0800

    user validations added, so name is necessary

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+
   it "has a name" do
     user = FactoryGirl.build(:user)
     expect(user.name).not_to be_nil
+
+    user = FactoryGirl.build(:user, name: nil)
+    expect(user).to be_invalid
+
+    user = FactoryGirl.build(:user, name: "")
+    expect(user).to be_invalid
   end
+  
   it "has a first name"
   it "has a last name"
   it "has an email address"
